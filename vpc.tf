827a0ecd58b823de2b7c27e64b982b05a44cef39
adv3006 <torikatok@gmail.com>
2022-06-06 17:10:30 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -4,11 +4,10 @@ module "vpc" {
 
     project_id   = ""
     network_name = "example-vpc"
-    routing_mode = "GLOBAL"
 
     subnets = [
         {
-            subnet_name           = "gaurav-subnet"
+            subnet_name           = "gaurav-subnet-3"
             subnet_ip             = "10.100.10.0/24"
             subnet_region         = var.region
         }
