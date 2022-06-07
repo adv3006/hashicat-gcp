33e3a12658fb93a07eda78f4cf071137bd7f9d75
Nhan PHan <torikatok@gmail.com>
2022-06-07 05:08:47 +0000

    Added vpc module

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "nhan-phan-ck-org"
     workspaces {
       name = "hashicat-gcp"
