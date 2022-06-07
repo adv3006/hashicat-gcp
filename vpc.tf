2e337e2f6aa6898540bf02a9cacbc006ef3f9879
Nhan PHan <torikatok@gmail.com>
2022-06-07 09:54:07 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,14 +1,14 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google"
-    version = "3.4.0"
+    version = "~> 4.0"
 
     project_id   = var.project
-    network_name = "gaurav-network-0000"
+    network_name = "gaurav-network-00000"
     routing_mode = "GLOBAL"
 
     subnets = [
         {
-            subnet_name           = "gaurav-subnet-001"
+            subnet_name           = "gaurav-subnet-0001"
             subnet_ip             = "10.100.10.0/24"
             subnet_region         = var.region
         }
